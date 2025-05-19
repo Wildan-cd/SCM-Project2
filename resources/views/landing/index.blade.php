bc25395a87cae501e943b106ce999b31ac7988ad
prtauliey <auliaprita04@gmail.com>
2025-05-19 11:25:02 +0700

    responsive

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1,3 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Septa Classic Motor</title>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link rel="shortcut icon" href="images/logo.png" type="images/logo.png">
--- resources/views/landing/tentang.blade.php
+++ resources/views/landing/tentang.blade.php
@@ -1,3 +1,5 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Septa Classic Motor</title>
 <link rel="stylesheet" href="{{ asset('css/tentang.css') }}">
 
